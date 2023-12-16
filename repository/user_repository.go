3fc2280fa3830494965e5e0b7d5c854a90fe76fd
Ryosuke <yoshimoto2107@gmail.com>
2023-12-16 20:58:15 +0900

    :sparkles:

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -19,6 +19,12 @@ func NewUserRepository(db *gorm.DB) IUserRepository {
 	return &userRepository{db}
 }
 
+
+// funcの宣言に(ur *userRepository)があるかないかで、
+// その関数が「メソッド」として振る舞うか、単なる「関数」として振る舞うかが変わります。
+// これはGo言語の「レシーバ」の概念に関連しています。
+// この記述があることで、GetUserByEmailはuserRepository型のインスタンスに紐付けられたメソッドとして定義されます。
+// メソッド内では、urを通じてレシーバのインスタンス（この場合はuserRepositoryのインスタンス）にアクセスできます。
 func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
 	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
 		return err
